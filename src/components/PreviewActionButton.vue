3f9b0222047e025aa720d7c89c5c0dc39502728f
yikoyu <2282373181@qq.com>
2025-03-12 14:57:29 +0800

    chore: update @tiptap dependencies to ^2.11.5 and enhance PreviewActionButton component

--- src/components/PreviewActionButton.vue
+++ src/components/PreviewActionButton.vue
@@ -5,21 +5,25 @@
   * @FilePath: \vuetify-pro-tiptap\examples\components\PreviewActionButton.vue
 -->
 <script setup lang="ts">
-import { ref } from 'vue'
-import { ActionButton } from 'vuetify-pro-tiptap'
-import { mdiClose, mdiFileCodeOutline } from '@mdi/js'
 import type { Editor } from '@tiptap/vue-3'
+import { mdiClose, mdiFileCodeOutline } from '@mdi/js'
+import { ref } from 'vue'
+import { useTheme } from 'vuetify'
 
-interface Props {
-  editor: Editor
-  tooltip?: string
-  disabled?: boolean
-}
+import { ActionButton } from 'vuetify-pro-tiptap'
 
 const props = withDefaults(defineProps<Props>(), {
   tooltip: undefined,
   disabled: false
 })
+
+// 获取原始主题
+const { global: { name: originalTheme } } = useTheme()
+interface Props {
+  editor: Editor
+  tooltip?: string
+  disabled?: boolean
+}
 
 const markdownTheme = ref('')
 const dialog = ref(false)
@@ -27,33 +31,35 @@ const maxWidth = ref<number>(900)
 </script>
 
 <template>
-  <ActionButton tooltip="全屏" :disabled="disabled">
+  <ActionButton :editor="editor" tooltip="全屏" :disabled="disabled">
     <VIcon>{{ `svg:${mdiFileCodeOutline}` }}</VIcon>
 
-    <VDialog v-model="dialog" fullscreen hide-overlay activator="parent">
-      <VCard>
-        <VToolbar dark color="primary">
-          <VBtn icon dark @click="dialog = false">
-            <VIcon>{{ `svg:${mdiClose}` }}</VIcon>
-          </VBtn>
+    <VThemeProvider :theme="originalTheme">
+      <VDialog v-model="dialog" fullscreen hide-overlay activator="parent">
+        <VCard>
+          <VToolbar dark color="primary">
+            <VBtn icon dark @click="dialog = false">
+              <VIcon>{{ `svg:${mdiClose}` }}</VIcon>
+            </VBtn>
 
-          <VBtnToggle v-model="markdownTheme" color="deep-purple-accent-3" rounded="0" group>
-            <VBtn value=""> Global </VBtn>
+            <VBtnToggle v-model="markdownTheme" color="deep-purple-accent-3" rounded="0" group>
+              <VBtn value=""> Global </VBtn>
 
-            <VBtn value="default"> Default </VBtn>
+              <VBtn value="default"> Default </VBtn>
 
-            <VBtn value="github"> Github </VBtn>
+              <VBtn value="github"> Github </VBtn>
 
-            <VBtn value="maidragon"> Maidragon </VBtn>
-          </VBtnToggle>
-        </VToolbar>
+              <VBtn value="maidragon"> Maidragon </VBtn>
+            </VBtnToggle>
+          </VToolbar>
 
-        <VContainer>
-          <VSheet class="mx-auto" :max-width="maxWidth">
-            <VuetifyViewer :value="editor.getJSON()" :markdown-theme="markdownTheme" />
-          </VSheet>
-        </VContainer>
-      </VCard>
-    </VDialog>
+          <VContainer>
+            <VSheet class="mx-auto" :max-width="maxWidth">
+              <VuetifyViewer :value="editor.getJSON()" :markdown-theme="markdownTheme" />
+            </VSheet>
+          </VContainer>
+        </VCard>
+      </VDialog>
+    </VThemeProvider>
   </ActionButton>
 </template>
